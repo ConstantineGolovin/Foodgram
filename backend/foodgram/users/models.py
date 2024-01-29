768a3f85abf436901de0e4845e32151bd2627cb7
Constantine <golwin163@yandex.ru>
2024-01-29 22:31:22 +0400

    Создание модели юзера

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(
+        'Логин',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150      
+    )
+    username = models.CharField(
+        'Ник пользователя',
+        max_length=150,
+        unique=True
+    )
+    email = models.EmailField(
+        'Email',
+        max_length=150,
+        unique=True
+    )
+    is_subscribed = models.BooleanField(
+        'Подписка',
+        default=False
+    )
+
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['username', 'email'],
+                name='unique_username_email'
+            )
+        ]
+
+    def __str__(self):
+        return self.username
