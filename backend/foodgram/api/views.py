1534bb883de391046f52ae0158caf97f887fdce5
Constantine <golwin163@yandex.ru>
2024-01-29 23:10:50 +0400

    Вьюсет ингредиентов

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from recipes.models import Ingredient
+from api.serializers import IngredientSerializers
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSets):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializers
