d9e30cd69421b748faf5d7de0ceb840c8745dbc7
Constantine <golwin163@yandex.ru>
2024-01-29 23:38:35 +0400

    Вьюсет и урл для тэга

--- backend/foodgram/api/ulrs.py
+++ backend/foodgram/api/ulrs.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet
+from api.views import IngredientViewSet, TagViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register('ingredients', IngredientViewSet, basename='ingredient')
+router_v1.register('tags', TagViewSet, basename='tag')
 
 
 urlpatterns = [
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from recipes.models import Ingredient
-from api.serializers import IngredientSerializers
+from recipes.models import Ingredient, Tag
+from api.serializers import IngredientSerializers, TagSerializers
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSets):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializers
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializers
