6a026700f00c7e124b7e48e59d68105b16f5faaf
Constantine <golwin163@yandex.ru>
2024-01-30 12:30:57 +0400

    Урл для рецепта

--- backend/foodgram/api/ulrs.py
+++ backend/foodgram/api/ulrs.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, TagViewSet
+from api.views import IngredientViewSet, TagViewSet, RecipeViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register('ingredients', IngredientViewSet, basename='ingredient')
 router_v1.register('tags', TagViewSet, basename='tag')
+router_v1.register('recipes', RecipeViewSet, basename='recipe')
 
 
 urlpatterns = [
