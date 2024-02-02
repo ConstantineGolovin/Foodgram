918425ab6c88d9f00aa61aa3830a2f668dda7745
Constantine <golwin163@yandex.ru>
2024-02-02 12:53:37 +0400

    Админка

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'username',
+        'email',
+    )
+    list_filter = ('first_name', 'email')
