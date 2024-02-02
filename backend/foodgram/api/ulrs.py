e1bed32eea183ade3901c5710f9fac4027795e6f
Constantine <golwin163@yandex.ru>
2024-02-02 10:39:01 +0400

    Добавление пути для юзера в урл

--- backend/foodgram/api/ulrs.py
+++ backend/foodgram/api/ulrs.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import IngredientViewSet, TagViewSet, RecipeViewSet
+from api.views import IngredientViewSet, TagViewSet, RecipeViewSet, UserViewSet
 
 
 app_name = 'api'
@@ -10,6 +10,7 @@ router_v1 = DefaultRouter()
 router_v1.register('ingredients', IngredientViewSet, basename='ingredient')
 router_v1.register('tags', TagViewSet, basename='tag')
 router_v1.register('recipes', RecipeViewSet, basename='recipe')
+router_v1.register('users', UserViewSet, basename='user')
 
 
 urlpatterns = [
