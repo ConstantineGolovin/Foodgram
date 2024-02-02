0461ec1f38361a6b5d3489dc528b79816fa88324
Constantine <golwin163@yandex.ru>
2024-02-02 19:23:04 +0400

    Изменение пути для статики

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -123,6 +123,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
