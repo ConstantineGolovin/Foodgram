db2b12d5d8221b96578c07cbf31794647aeddf0b
Constantine <golwin163@yandex.ru>
2024-02-02 16:25:05 +0400

    Исправление ошибок сборки контейнеров

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/django/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static_django/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
