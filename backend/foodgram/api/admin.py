314025ac910c302bfcca74e2ea5305a7508ec1f5
Constantine <golwin163@yandex.ru>
2024-02-02 20:50:48 +0400

    Преображение админки

---
+++ backend/foodgram/api/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+from recipes.models import (Tag, Ingredient, Favorite,
+                            Recipe, CountIngredientInRecipe,
+                            ShoppingCart)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name',)
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'author', 'favorite',)
+    list_filter = ('author', 'name', 'tags',)
+
+    def favorite(self, obj):
+        return obj.favorite.count()
+
+
+@admin.register(CountIngredientInRecipe)
+class CountIngredientInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount',)
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'user',)
