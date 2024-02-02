44030d379b465c02cb7918d2eb45bf2c7c7d85b9
Constantine <golwin163@yandex.ru>
2024-02-02 13:39:34 +0400

    Создание фильтра для ингредиента

---
+++ backend/foodgram/api/filters.py
@@ -0,0 +1,11 @@
+from django_filters.rest_framework import FilterSet, filters 
+
+from recipes.models import Ingredient
+
+
+class IngredientFilter(FilterSet):
+    name = filters.CharFilter(lookup_expr='istartswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
