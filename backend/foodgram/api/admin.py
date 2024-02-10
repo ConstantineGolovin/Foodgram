f5e216aa24990a072db7603607d0be8c105c2b61
Constantine <golwin163@yandex.ru>
2024-02-10 12:10:06 +0400

    Изменение админки

--- backend/foodgram/api/admin.py
+++ backend/foodgram/api/admin.py
@@ -1,22 +1,32 @@
-from django.contrib.admin import ModelAdmin, register, site
+from django.contrib.admin import ModelAdmin, TabularInline, register, site
+from django.utils.safestring import mark_safe
 
 from recipes.models import (Tag, Ingredient, Favorite,
-                            Recipe, CountIngredientInRecipe,
-                            ShoppingCart)
+                            Recipe, ShoppingCart)
 
 site.site_header = 'Администрирование Foodgram'
 EMPTY_VALUE_DISPLAY = 'Значение не указано'
 
 
+class IngredientInLine(TabularInline):
+    model = Recipe.ingredients.through
+    extra = 2
+    min_num = 1
+
+
 @register(Tag)
 class TagAdmin(ModelAdmin):
-    list_display = ('name', 'slug', 'color')
+    list_display = ('name', 'slug', 'color',)
+    empty_value_display = EMPTY_VALUE_DISPLAY
+    search_fields = ('name', 'color')
 
 
 @register(Ingredient)
 class IngredientAdmin(ModelAdmin):
     list_display = ('name', 'measurement_unit',)
     list_filter = ('name',)
+    search_fields = ('name',)
+    empty_value_display = EMPTY_VALUE_DISPLAY
 
 
 @register(Favorite)
@@ -26,16 +36,35 @@ class FavoriteAdmin(ModelAdmin):
 
 @register(Recipe)
 class RecipeAdmin(ModelAdmin):
-    list_display = ('name', 'id', 'author', 'favorite',)
-    list_filter = ('author', 'name', 'tags',)
+    list_display = ('name', 'author', 'get_image',
+                    'get_ingredients', 'get_count_favourites')
+    list_filter = ('author__username', 'name', 'tags',)
+    fields = (
+        ('name', 'cooking_time',),
+        ('author', 'tags',),
+        ('text',),
+        ('image',),
+    )
+    raw_id_fields = ('author',)
+    search_fields = ('name', 'author__username', 'tags',)
+    inlines = (IngredientInLine,)
+    empty_valuse_display = EMPTY_VALUE_DISPLAY
 
-    def favorite(self, obj):
+    def get_ingredients(self, obj):
+        return ', '.join(
+            ingredient.ingredient.name
+            for ingredient in obj.countingredientinrecipe.all()
+        )
+
+    def get_count_favourites(self, obj):
         return obj.favorites.count()
 
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="80" height="30"')
 
-@register(CountIngredientInRecipe)
-class CountIngredientInRecipeAdmin(ModelAdmin):
-    list_display = ('recipe', 'ingredient', 'amount',)
+    get_image.short_description = 'Изображение'
+    get_count_favourites.short_description = 'Добавили в избранное'
+    get_ingredients.short_description = 'Ингредиенты'
 
 
 @register(ShoppingCart)
