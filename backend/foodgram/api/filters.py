9869431003c3f44ba661cd1dbebdeb0e948a8ad4
Constantine <golwin163@yandex.ru>
2024-02-10 12:38:07 +0400

    Исправил фильтрацию

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -17,8 +17,8 @@ class RecipeFilter(FilterSet):
         to_field_name='slug',
         queryset=Tag.objects.all()
     )
-    favorite = filters.BooleanFilter(method='filter_favorite')
-    shopping_cart = filters.BooleanFilter(method='filter_shopping_cart')
+    is_favorites = filters.BooleanFilter(method='filter_favorite')
+    is_in_shopping_cart = filters.BooleanFilter(method='filter_shopping_cart')
 
     class Meta:
         model = Recipe
@@ -26,9 +26,10 @@ class RecipeFilter(FilterSet):
 
     def filter_favorite(self, name, queryset, value):
         if value:
-            return queryset.filter(favorite__user=self.request.user)
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
     def filter_shopping_cart(self, name, queryset, value):
         if value:
             return queryset.filter(shoppingcart__user=self.request.user)
+        return queryset
