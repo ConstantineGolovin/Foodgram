bde7f6035bd27bbb750c2611d53a7bfec0baa0ae
Constantine <golwin163@yandex.ru>
2024-02-16 02:03:59 +0400

    Исправил корзину с покупками

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -102,11 +102,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
             'ingredient__name',
             'ingredient__measurement_unit'
         ).annotate(amount=Sum('amount')).order_by('ingredient__name')
-        shopping_cart = 'Список покупок'
+        shopping_cart = 'Список покупок\n'
         shopping_cart += ''.join([
             f'{ingredient["ingredient__name"]}'
-            f'{ingredient["ingredient__measurement_unit"]}'
-            f'{ingredient["amount"]}'
+            f'{ingredient["ingredient__measurement_unit"]} '
+            f'{ingredient["amount"]}\n'
             for ingredient in ingredients
         ])
         response = HttpResponse(shopping_cart, content_type='text/plan')
